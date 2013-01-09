1e93ccaec4bbd1356ca4161d624c8ae409192eb0
Jan Tymiński <tymikk@gmail.com>
2013-01-09 01:49:16 +0100

    first version of inverting assembler function. must organize loop, fix jump, handle size of image...

---
+++ invert.asm
@@ -0,0 +1,41 @@
+global asm_invert:function
+
+asm_invert:
+	mov r8, rdi ; array pointer here
+
+	mov r9, 255 ; 255 for subtracting
+	mov r10, [r8] ; value of dword
+	shr r10, 24 ; getting first byte of dword
+	and r10, 0xff ; mask
+	sub r9, r10 ; color inversion, 255-value
+	shl r9, 24 ; move byte to start of dword
+	and r9, 0xff000000 ; another mask
+	mov r11, r9 ; move to temp and first pixel here is done
+
+	mov r9, 255 ; 255 for subtracting
+	mov r10, [r8] ; value of dword
+	shr r10, 16 ; getting second byte of dword
+	and r10, 0xff ; mask
+	sub r9, r10 ; color inversion, 255-value
+	shl r9, 16 ; move byte to start of dword
+	and r9, 0x00ff0000 ; another mask
+	add r11, r9 ; move to temp and second pixel here is done
+
+	mov r9, 255 ; 255 for subtracting
+	mov r10, [r8] ; value of dword
+	shr r10, 8 ; getting third byte of dword
+	and r10, 0xff ; mask
+	sub r9, r10 ; color inversion, 255-value
+	shl r9, 8 ; move byte to start of dword
+	and r9, 0x0000ff00 ; another mask
+	add r11, r9 ; move to temp and third pixel here is done
+
+	mov r9, 255 ; 255 for subtracting
+	mov r10, [r8] ; value of dword
+	and r10, 0xff ; mask - getting last byte of dword
+	sub r9, r10 ; color inversion, 255-value
+	and r9, 0xff ; another mask
+	add r11, r9 ; move to temp and last pixel here is done, so dword done, can loop
+
+	mov [r8], r11 ; put changed dword back to it's place
+	jmp asm_invert ; this jump has to consider length, not done now
