b0165e85bfeaa178828a0d4ed8edb82630564621
Jan Tymiński <tymikk@gmail.com>
2013-01-15 00:34:02 +0100

    at least less errors...

--- io.cpp
+++ io.cpp
@@ -3,7 +3,8 @@
 void Image::load(std::string img_path) {
 	if (image)
 		delete image;
-	image = & CImg::load<unsigned char> (img_path);
+	//image = & CImg::load<unsigned char> (img_path);
+	image = new CImg<unsigned char> (img_path.c_str());
 }
 
 void Image::save() {} ;
