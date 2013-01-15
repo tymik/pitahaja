4b1a408cb2405e434b8688c92d819d94fdc3cf56
Jan Tymiński <tymikk@gmail.com>
2013-01-15 01:12:36 +0100

    finally it does compile, thanks to my friend. tribute to him. TODO: fix assembler algorithm.

--- io.cpp
+++ io.cpp
@@ -3,7 +3,6 @@
 void Image::load(std::string img_path) {
 	if (image)
 		delete image;
-	//image = & CImg::load<unsigned char> (img_path);
 	image = new CImg<unsigned char> (img_path.c_str());
 }
 
@@ -11,5 +10,5 @@ void Image::save() {} ;
 
 void Image::run_func( void (*func)(unsigned char*, int) ){
 	if (image)
-		func( image.data(), image.size() );
+		func( image->data(), image->size() );
 }
