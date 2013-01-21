f94355b44f81de527170a648f5a9d7e770f763e9
Jan Tymiński <tymikk@gmail.com>
2013-01-21 02:06:16 +0100

    created macro and testes that i did it right. got to cleanup rest of code to fit the macro. it still does not work, but now it will be much easier to make further tests.

--- invert.asm
+++ invert.asm
@@ -1,10 +1,70 @@
 global asm_invert:function
 
+%macro invert_pixel 1
+        mov r9, 255 ; 255 for subtracting
+        mov r10, [r8] ; value of dword
+        shr r10, 8*%1 ; getting first byte of dword
+        and r10, 0xff ; mask
+        sub r9, r10 ; color inversion, 255-value
+        shl r9, 8*%1 ; move byte to start of dword
+        and r9, 0xff<<8*%1 ; another mask
+        mov r11, r9 ; move to temp and first pixel here is done
+        sub r15, 1 ; subtract from size - loop exit condition
+        jz inverted ; jump if all pixels are processed
+%endmacro
+
 asm_invert:
 	mov r8, rdi ; array pointer here
 	mov r15, rsi ; image size here
 
 invert:
+;	mov r9, 255 ; 255 for subtracting
+;	mov r10, [r8] ; value of dword
+;	shr r10, 8*7 ; getting first byte of dword
+;	and r10, 0xff ; mask
+;	sub r9, r10 ; color inversion, 255-value
+;	shl r9, 8*7 ; move byte to start of dword
+;	and r9, 0xff<<8*7 ; another mask
+;	mov r11, r9 ; move to temp and first pixel here is done
+;	sub r15, 1 ; subtract from size - loop exit condition
+;	jz inverted ; jump if all pixels are processed
+;
+;	mov r9, 255 ; 255 for subtracting
+;	mov r10, [r8] ; value of dword
+;	shr r10, 8*6 ; getting second byte of dword
+;	and r10, 0xff ; mask
+;	sub r9, r10 ; color inversion, 255-value
+;	shl r9, 8*6 ; move byte to second octet of dword
+;	and r9, 0xff<<8*6 ; another mask
+;	add r11, r9 ; move to temp and second pixel here is done
+;	sub r15, 1 ; subtract from size - loop exit condition
+;	jz inverted ; jump if all pixels are processed
+;
+;	mov r9, 255 ; 255 for subtracting
+;	mov r10, [r8] ; value of dword
+;	shr r10, 8*5 ; getting first byte of dword
+;	and r10, 0xff ; mask
+;	sub r9, r10 ; color inversion, 255-value
+;	shl r9, 8*5 ; move byte to start of dword
+;	and r9, 0xff<<8*5 ; another mask
+;	add r11, r9 ; move to temp and first pixel here is done
+;	sub r15, 1 ; subtract from size - loop exit condition
+;	jz inverted ; jump if all pixels are processed
+;
+	invert_pixel 7
+	invert_pixel 6
+	invert_pixel 5
+	mov r9, 255 ; 255 for subtracting
+	mov r10, [r8] ; value of dword
+	shr r10, 8*4 ; getting second byte of dword
+	and r10, 0xff ; mask
+	sub r9, r10 ; color inversion, 255-value
+	shl r9, 8*4 ; move byte to second octet of dword
+	and r9, 0xff<<8*4 ; another mask
+	add r11, r9 ; move to temp and second pixel here is done
+	sub r15, 1 ; subtract from size - loop exit condition
+	jz inverted ; jump if all pixels are processed
+
 	mov r9, 255 ; 255 for subtracting
 	mov r10, [r8] ; value of dword
 	shr r10, 8*3 ; getting first byte of dword
@@ -50,7 +110,7 @@ invert:
 inverted:
 	mov [r8], r11 ; put changed dword back to it's place
 
-	add r8, 4 ; move to next dword
+	add r8, 8 ; move to next dword
 	sub r15, 1 ; subtract from size - loop exit condition
 	jnbe invert ; this jump has to consider length, not done now
 
