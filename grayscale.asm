6a7c0a82e38816c9a949aaae9d9ce0160c52410e
Jan Tymiński <tymikk@gmail.com>
2013-01-23 22:32:13 +0100

    reorganized pixel conversion code, added norm_{red,green,blue} macros. code is not clean yet. also no looping yet. not ready for tests.

--- grayscale.asm
+++ grayscale.asm
@@ -1,6 +1,67 @@
 global asm_grayscale:function
 
+%macro norm_red 0
+	xor r11, r11
+	mov r8b, [r13]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
 
+	mov r8b, [r13+r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+
+	mov r8b, [r13+2*r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+%endmacro
+
+%macro norm_green 0
+	xor r11, r11
+	mov r8b, [r13-r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+
+	mov r8b, [r13]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+
+	mov r8b, [r13+r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+%endmacro
+
+%macro norm_blue 0
+	xor r11, r11
+	mov r8b, [r13-2*r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+
+	mov r8b, [r13-r14]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+
+	mov r8b, [r13]
+	mov r12, r8
+	shr r12, 8*7
+	and r12, 0xff
+	add r11, r12
+%endmacro
 
 asm_grayscale:
         mov r13, rdi ; array pointer here
@@ -10,22 +71,10 @@ asm_grayscale:
 	div rdx 
 	mov r14, rax ;
 
-	mov r8b, [r13]
-	mov r9b, [r13+r14]
-	mov r10b, [r13+2*r14]
-	xor r11, r11
-	mov r12, r8
-	shr r12, 8*7
-	and r12, 0xff
-	add r11, r12
-	mov r12, r9
-	shr r12, 8*7
-	and r12, 0xff
-	add r11, r12
-	mov r12, r10
-	shr r12, 8*7
-	and r12, 0xff
-	add r11, r12
-	
+	mov r10, r14
+red:
+	norm_red
+
+	mov [r8b], r11
 
 	ret ; return from function
