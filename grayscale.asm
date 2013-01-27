58561df2a73ed79758fa98702c21ca9bc563e331
Jan Tymiński <tymikk@gmail.com>
2013-01-27 02:42:57 +0100

    some cleanup. norm_red left as a pattern, or a kind of tip for incoming changes. and already used it to fix my stupidity in previously provided norm_pixel. also partially added to main code. got to loop it and handle with images of size%8!=0

--- grayscale.asm
+++ grayscale.asm
@@ -1,12 +1,33 @@
 global asm_grayscale:function
 
 %macro norm_pixel 1 ; must xor r11, r11 before call norm_pixel 7
-	mov r12, [r13]
-	and r12, 0xff<<8*%7
-	add r11, r12
+	xor rax, rax
+
+	mov r12, [r13] ; red
+	shr r12, 8*%1
+	and r12, 0xff
+	add rax, r12
+
+	mov r12, [r13+r14] ; green
+	shr r12, 8*%1
+	and r12, 0xff
+	add rax, r12
+
+	mov r12, [r13+r14*2] ; blue
+	shr r12, 8*%1
+	and r12, 0xff
+	add rax, r12
+
+	cdq ; make rax qword, so div can work
+	xor rbx, rbx
+	mov rbx, 3
+	div rbx
+	add r11, rax ; add average to almost final destination
+
+	sub r10, 1 ; decrease the counter
 %endmacro
 
-%macro put_full_reg 1 ; takes value of [r13] and puts normalised pixel in 3 places
+%macro put_full_reg 0 ; takes value of r11 and puts normalised pixel in 3 places
 	mov [r13], r11
 	mov [r13+r14], r11
 	mov [r13+r14*2], r11
@@ -40,68 +61,6 @@ global asm_grayscale:function
 	mov [r13], r11
 %endmacro
 
-%macro norm_green 0
-	xor r11, r11
-	neg r14
-	mov r8b, [r13+r14] ; -1*r14
-	neg r14
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov r8b, [r13]
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov r8b, [r13+r14]
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov rax, r11
-	cdq
-	xor rbx, rbx
-	mov rbx, 3
-	div rbx
-	mov r11, rax
-
-	shl r11, 8*7
-	mov [r13], r11
-%endmacro
-
-%macro norm_blue 0
-	xor r11, r11
-	neg r14
-	mov r8b, [r13+r14*2] ; -2*r14
-	neg r14
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	neg r14
-	mov r8b, [r13+r14] ; -1*r14
-	neg r14
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov r8b, [r13]
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov rax, r11
-	cdq
-	xor rbx, rbx
-	mov rbx, 3
-	div rbx
-	mov r11, rax
-
-	shl r11, 8*7
-	mov [r13], r11
-%endmacro
-
 asm_grayscale:
         mov r13, rdi ; array pointer here
         mov r15, rsi ; image size here
@@ -113,25 +72,14 @@ asm_grayscale:
 	mov r14, rax ; here size/3 is received, for offset usage
 
 	mov r10, r14 ; for the loop counter r10 is used
-red:
-	norm_red
-	add r13, 1
-	sub r10, 1
-	jnz red
-
-	mov r10, r14
-green:
-	norm_green
-	add r13, 1
-	sub r10, 1
-	jnz green
-
-	mov r10, r14
-blue:
-	norm_blue
-	add r13, 1
-	sub r10, 1
-	jnz blue
-
+full_reg:
+	norm_pixel 7
+	norm_pixel 6
+	norm_pixel 5
+	norm_pixel 4
+	norm_pixel 3
+	norm_pixel 2
+	norm_pixel 1
+	norm_pixel 0
 
 	ret ; return from function
