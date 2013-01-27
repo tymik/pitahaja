742902cb184d6d3041e52f5263fef5327684c08e
Jan Tymiński <tymikk@gmail.com>
2013-01-27 15:24:36 +0100

    got to finish norm_unfull. quick commit, g2g

--- grayscale.asm
+++ grayscale.asm
@@ -31,34 +31,22 @@ global asm_grayscale:function
 	mov [r13], r11
 	mov [r13+r14], r11
 	mov [r13+r14*2], r11
+	add r13, 8
 %endmacro
 
-%macro norm_red 0
-	xor r11, r11
-	mov r8b, byte [r13]
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
+%macro norm_unfull 1 ; normalization for images with dimension not being multiply of 8
+	xor rax, rax
+	xor rcx, rcx
 
-	mov r8b, [r13+r14]
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
+	mov rax, [r13]
+	mov rcx, [r13]
+	mov rdx, 0xFFFFFFFFFFFFFFFF
 
-	mov r8b, [r13+r14*2] ; 2*r14
-	mov r12, r8
-	and r12, 0xff
-	add r11, r12
-
-	mov rax, r11
-	cdq
-	xor rbx, rbx
-	mov rbx, 3
-	div rbx
-	mov r11, rax
-
-	shl r11, 8*7
-	mov [r13], r11
+	neg %1
+	shr rax, 8*8+%1*8
+	shr rdx, 8*8+%1*8
+	and rax, rdx
+	neg %1
 %endmacro
 
 asm_grayscale:
@@ -72,6 +60,7 @@ asm_grayscale:
 	mov r14, rax ; here size/3 is received, for offset usage
 
 	mov r10, r14 ; for the loop counter r10 is used
+
 full_reg:
 	norm_pixel 7
 	norm_pixel 6
@@ -82,4 +71,12 @@ full_reg:
 	norm_pixel 1
 	norm_pixel 0
 
-	ret ; return from function
+	put_full_reg
+
+	cmp r10, 8
+	jnb full_reg ; if there is still at least one full register to convert
+	jz end ; if there were only full registers to be processed
+
+	norm_unfull r10
+
+end:	ret ; return from function
